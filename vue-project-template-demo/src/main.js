71922b72de359bdecc6f633c506d8c19f1d6e533
dawangong <dawangong@126.com>
2018-08-02 15:18:47 +0800

    json-server 和 vue-resource的加入

--- vue-project-template-demo/src/main.js
+++ vue-project-template-demo/src/main.js
@@ -6,6 +6,17 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import router from './router';
 import './common/styles/reset.less';
+import VueResource from 'vue-resource';
+const DEFAULT_API_PREFIX = '';
+// 拦截增加请求默认前缀
+Vue.use(VueResource);
+Vue.http.interceptors.push((req, next) => {
+  let currentUrl = req.url;
+  currentUrl = currentUrl.startsWith('/') ? currentUrl : `/${currentUrl}`;
+  if (!currentUrl.startsWith(DEFAULT_API_PREFIX)) {
+    req.url = `${DEFAULT_API_PREFIX}${currentUrl}`;
+  }
+});
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 /* eslint-disable no-new */
